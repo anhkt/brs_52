3c0861959b5dd36895537200392b56afc070e0dd
Diemly09 <diemly0902@gmail.com>
2017-02-07 15:10:56 +0700

    Create category

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,7 @@ Rails.application.routes.draw do
   delete "/logout", to: "sessions#destroy"
 
   resources :users, only: [:new, :create, :show]
+  namespace :admins do
+    resources :categories, only: [:index, :create]
+  end
 end
