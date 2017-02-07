df0f65ef5677d6e0f90487960966b4d3a5bb4fbe
NguyenHuyLong <nguyen.huy.long@framgia.com>
2017-02-07 16:09:36 +0700

    Show user profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :find_user, only: :show
+
   def new
     @user = User.new
   end
@@ -13,8 +15,19 @@ class UsersController < ApplicationController
     end
   end
 
+  def show
+  end
+
   private
   def user_params
     params.require(:user).permit :name, :email, :password, :password_confirmation
+  end
+
+  def find_user
+    @user = User.find_by id: params[:id]
+    if @user.nil?
+      flash[:danger] = t "flash.danger.user_not_exist"
+      redirect_to root_path
+    end
   end
 end
