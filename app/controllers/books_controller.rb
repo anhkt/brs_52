f8d7e502ab46d4b421d50614e77597e869ec8eee
NguyenHuyLong <nguyen.huy.long@framgia.com>
2017-02-10 15:51:05 +0700

    Search book by title or author

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,12 +1,13 @@
 class BooksController < ApplicationController
   def index
-    @books = Book.all
+    @categories = Category.all
+    @books = Book.search params[:search_param], params[:category_id]
   end
 
   def show
     @book = Book.find_by id: params[:id]
-    if @book.nil?
-      flash[:danger] = t "flash.danger.book_not_exist"
+    unless @book
+      flash[:danger] = t "flash.danger.book_not_found"
       redirect_to books_path
     end
   end
--- app/models/book.rb
+++ app/models/book.rb
@@ -3,4 +3,20 @@ class Book < ApplicationRecord
 
   has_many :reviews
   has_many :userbooks
+
+  scope :search_book_by_search_params, -> search_param do
+    where "title LIKE ? OR author LIKE ?",
+      "%#{search_param}%", "%#{search_param}%" if search_param.present?
+  end
+  scope :order_by_id, -> {order id: :desc}
+
+  class << self
+    def search search_param, category_id
+      if category_id.present?
+        Category.find_by(id: category_id).books
+      else
+        all
+      end.search_book_by_search_params(search_param).order_by_id
+    end
+  end
 end
