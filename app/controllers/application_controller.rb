093c96a781c0989bb393d6a5cebb420b60550d80
TrungNT <hellovietnam93@gmail.com>
2017-05-10 10:31:44 +0700

    Multi language

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protect_from_forgery with: :exception
-  before_action :authenticate_user!
+  before_action :authenticate_user!, :set_locale
 
   include ApplicationHelper
 
@@ -17,5 +17,13 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up) do |user_params|
       user_params.permit :name, :email, :password, :password_confirmation
     end
+  end
+
+  def default_url_options options = {}
+    {locale: I18n.locale}
+  end
+
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
   end
 end
