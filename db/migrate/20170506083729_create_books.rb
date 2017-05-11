457165adcfdc238dad653738a72d885f1a7aa7fa
Luong Van Sang <sanga2k50@gmail.com>
2017-05-12 05:22:50 +0700

    Fix show rating nil

--- db/migrate/20170506083729_create_books.rb
+++ db/migrate/20170506083729_create_books.rb
@@ -8,7 +8,7 @@ class CreateBooks < ActiveRecord::Migration[5.0]
       t.datetime :publish_date
       t.integer :number_of_pages
       t.integer :category_id
-      t.float :rating
+      t.float :rating, default: 0
 
       t.timestamps
     end
